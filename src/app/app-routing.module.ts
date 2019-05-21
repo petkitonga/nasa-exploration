e65bcfd922a56b23e50a70abf40ccb2199ed44ef
Peter Kitonga <petkitonga@gmail.com>
2019-05-21 21:31:16 +0300

    Initial packages and components setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ApodComponent } from './apod/apod.component';
+import { AsteroidsComponent } from './asteroids/asteroids.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ApodComponent },
+  { path: 'apod', component: ApodComponent },
+  { path: 'asteroids', component: AsteroidsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsteroidsComponent } from './asteroids/asteroids.component';
+import { ApodComponent } from './apod/apod.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsteroidsComponent,
+    ApodComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
